1cfae1e7d3333130b74e834705222408019a1ad5
cassandralima <cassandralima15@hotmail.com>
2025-09-15 10:28:17 -0300

    ajuste

--- csharp/obj/Debug/net8.0/csharp.AssemblyInfo.cs
+++ csharp/obj/Debug/net8.0/csharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("csharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59833b8156abe3b26c9b72349ea9705faef4e060")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2108cf972d4c39ef70abb2c230d8b71a5c6a067b")]
 [assembly: System.Reflection.AssemblyProductAttribute("csharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("csharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
